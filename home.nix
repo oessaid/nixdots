12bd377ae9adabb15cc5e162c44f589403990ac3
Omar Essaid <omar.essaid@gmail.com>
2024-02-21 12:48:46 +0100

    Basic working setup

--- home.nix
+++ home.nix
@@ -12,7 +12,7 @@
   home.packages = with pkgs; [
     neofetch
     nnn # terminal file manager
-    jetbrains-mono # font
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
     # utils
     ripgrep # recursively searches directories for a regex pattern
@@ -214,8 +214,19 @@
     # custom settings
     settings = {
       env.TERM = "xterm-256color";
+      scrolling.history = 10000;
       font = {
         size = 12.0;
+	normal.family = "JetBrainsMono Nerd Font";
+	bold.family = "JetBrainsMono Nerd Font";
+	italic.family = "JetBrainsMono Nerd Font";
+      };
+      window = {
+        dynamic_padding = true;
+        padding = {
+	  x = 2;
+	  y = 2;
+	};
       };
       #scrolling.multiplier = 5;
       #selection.save_to_clipboard = true;
